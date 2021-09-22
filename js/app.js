e2be9d87a2720fd7800d7b3761e5cc360ef68727
RodriBlanca <rodrigoblanca96@gmail.com>
2021-09-22 19:02:05 -0300

    Últimas modificaciones

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 /* VARIABLES */
-// Botones plan mensual o anual
+// MONTHLY AND ANNUALY BUTTONS
 const monthlyBtn = document.getElementById('monthly');
 const annualyBtn = document.getElementById('annualy');
-// Información de los planes en las tarjetas
+// PACKAGE CARD INFORMATION
 const basicPrice = document.getElementById('basic-price');
 const premiumPrice = document.getElementById('premium-price');
 const businessPrice = document.getElementById('business-price');
@@ -10,7 +10,6 @@ const packageOption = document.getElementsByClassName('price-month');
 const cardButton = document.getElementsByClassName('card-description__button');
 
 /* EVENTS */
-// Carga los eventListenners
 const loadEventListenners = () => {
     monthlyBtn.addEventListener('click', monthlyOption)
 
@@ -20,58 +19,40 @@ const loadEventListenners = () => {
 loadEventListenners();
 
 /* FUNCTIONS */
-// Establece la opción mensual y aplica los cambios al DOM
+// SET MONTHLY CHANGES AND APPLY DOM CHANGES
 function monthlyOption() {
 
-    // Establece el precio mensual del paquete Basic
     basicPrice.textContent = `$39`;
-
-    // Establece el precio anual del paquete Premium
     premiumPrice.textContent = `$49`;
-
-    // Establece el precio anual del paquete Business
     businessPrice.textContent = `$59`;
 
-    // Modifica el span de las tarjetas a mensual
     for(let i = 0; i < packageOption.length; i++) {
         packageOption[i].textContent = `/Monthly`;
     }
 
-    // Cambio de color del botón
-    monthlyBtn.style.backgroundColor = '#26C281';
-    monthlyBtn.style.color = '#FFFFFF';
-    annualyBtn.style.backgroundColor = '#E7FAF4';
-    annualyBtn.style.color = '#26C281';
-
-    // Efecto para el botón no seleccionado
-    monthlyBtn.classList.remove('none-selected');
-    annualyBtn.classList.add('none-selected');
+    if(monthlyBtn.classList.contains('none-selected')) {
+        monthlyBtn.classList.remove('none-selected');
+        monthlyBtn.classList.add('selected-button');
+        annualyBtn.classList.remove('selected-button');
+        annualyBtn.classList.add('none-selected');
+    }
 }
 
-// Establece la opción anual y aplica los cambios al DOM
+// SET ANNUALY CHANGES AND APPLY DOM CHANGES
 function annualyOption() {
 
-    // Establece el precio anual del paquete Basic
     basicPrice.textContent = `$399`;
-
-    // Establece el precio anual del paquete Premium
     premiumPrice.textContent = `$499`;
-
-    // Establece el precio anual del paquete Business
     businessPrice.textContent = `$599`;
 
-    // Modifica el span de las tarjetas a anual
     for(let i = 0; i < packageOption.length; i++) {
         packageOption[i].textContent = `/Annualy`;
     }
 
-    // Cambio de color del botón
-    annualyBtn.style.backgroundColor = '#26C281';
-    annualyBtn.style.color = '#FFFFFF';
-    monthlyBtn.style.backgroundColor = '#E7FAF4';
-    monthlyBtn.style.color = '#26C281';
-
-    // Efecto para el botón no seleccionado
-    annualyBtn.classList.remove('none-selected');
-    monthlyBtn.classList.add('none-selected');
+    if(annualyBtn.classList.contains('none-selected')) {
+        annualyBtn.classList.remove('none-selected');
+        annualyBtn.classList.add('selected-button');
+        monthlyBtn.classList.remove('selected-button');
+        monthlyBtn.classList.add('none-selected');
+    }
 }
